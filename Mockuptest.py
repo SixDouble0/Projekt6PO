ee4e1e4d134b44d0b041938559e49a7d77e712ae
SixDouble0 <162843064+SixDouble0@users.noreply.github.com>
2024-06-11 00:03:16 +0200

    Update Mockuptest.py

--- Mockuptest.py
+++ Mockuptest.py
@@ -1,72 +1,79 @@
 import unittest
-from Function import Sala, System, Inwentaryzacja
-
+from Function import System, Sala
 
 class TestSystem(unittest.TestCase):
+
     def setUp(self):
         self.system = System()
 
-
-        self.sala1 = Sala(1, [Sala.Sprzet(1, "Komputer"), Sala.Krzeslo(2, "Krzeslo")])
-        self.sala2 = Sala(2, [Sala.Sprzet(3, "Projektor"), Sala.Stol(4, "Stol")])
-
-
-        self.system.dodaj_sale(self.sala1)
-        self.system.dodaj_sale(self.sala2)
-
     def test_dodaj_sale(self):
-        self.assertEqual(len(self.system.sale), 2)
-        self.assertEqual(self.system.sale[0].Numer, 1)
-        self.assertEqual(self.system.sale[1].Numer, 2)
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.assertIn(sala1, self.system.sale)
 
     def test_usun_sale(self):
-        self.system.usun_sale(1)
-        self.assertEqual(len(self.system.sale), 1)
-        self.assertEqual(self.system.sale[0].Numer, 2)
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.system.usun_sale(101)
+        self.assertNotIn(sala1, self.system.sale)
 
-    def test_przegladaj_sale(self):
-        self.system.przegladaj_sale()
+    def test_dodaj_skladnik(self):
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_skladnik(101, 'sprzet', 'Komputer', 'Nowy')
+        self.assertEqual(len(sala1.skladniki), 1)
+        self.assertEqual(sala1.skladniki[0].nazwa, 'Komputer')
 
-    def test_znajdz_skladnik(self):
-        wynik = self.system.znajdz_skladnik("Komputer")
-        self.assertEqual(len(wynik), 1)
-        self.assertEqual(wynik[0].nazwa, "Komputer")
+    def test_usun_skladnik(self):
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_skladnik(101, 'sprzet', 'Komputer', 'Nowy')
+        skladnik_id = sala1.skladniki[0].id
+        self.system.usun_skladnik(101, skladnik_id)
+        self.assertEqual(len(sala1.skladniki), 0)
 
-        wynik = self.system.znajdz_skladnik("Projektor", Sala.Sprzet)
-        self.assertEqual(len(wynik), 1)
-        self.assertEqual(wynik[0].nazwa, "Projektor")
-
-    def test_przenies_skladnik(self):
-        self.system.przenies_skladnik("Komputer", 1, 2)
-        self.assertEqual(len(self.sala1.skladniki.skladniki), 1)
-        self.assertEqual(len(self.sala2.skladniki.skladniki), 3)
+    def test_wyszukaj_skladnik(self):
+        sala1 = Sala(101)
+        sala2 = Sala(102)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_sale(sala2)
+        self.system.dodaj_skladnik(101, 'meble', 'Krzesło', 'Dobry')
+        self.system.dodaj_skladnik(102, 'meble', 'Krzesło', 'Zły')
+        wyniki = self.system.wyszukaj_skladnik('Krzesło')
+        self.assertEqual(len(wyniki), 2)
 
     def test_inwentaryzacja(self):
-        skladniki_do_sprawdzenia = [Sala.Sprzet(1, "Komputer"), Sala.Krzeslo(2, "Krzeslo")]
-        inwentaryzacja = self.system.inwentaryzacja(1, skladniki_do_sprawdzenia)
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_skladnik(101, 'sprzet', 'Komputer', 'Nowy')
+        raport = self.system.inwentaryzacja()
+        self.assertIn('Brakujące składniki: Krzesło, Biurko', raport)
 
-        self.assertIsInstance(inwentaryzacja, Inwentaryzacja)
-        self.assertEqual(len(inwentaryzacja.braki), 0)
-        self.assertEqual(len(inwentaryzacja.kiepski_stan), 0)
-        self.assertEqual(len(inwentaryzacja.nowe), 0)
+    def test_zapisz_i_wczytaj_raport(self):
+        sala1 = Sala(101)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_skladnik(101, 'sprzet', 'Komputer', 'Nowy')
+        self.system.zapisz_raport('raport.txt')
 
-        skladniki_do_sprawdzenia = [Sala.Sprzet(1, "Komputer", "zły"), Sala.Krzeslo(2, "Krzeslo")]
-        inwentaryzacja = self.system.inwentaryzacja(1, skladniki_do_sprawdzenia)
 
-        self.assertEqual(len(inwentaryzacja.kiepski_stan), 1)
-        self.assertEqual(inwentaryzacja.kiepski_stan[0].stan, "zły")
+        nowy_system = System()
+        nowy_system.wczytaj_raport('raport.txt')
+        self.assertEqual(len(nowy_system.sale), 1)
+        self.assertEqual(nowy_system.sale[0].numer, 101)
+        self.assertEqual(nowy_system.sale[0].skladniki[0].nazwa, 'Komputer')
 
-    def test_zapisz_odczytaj_z_pliku(self):
-        self.system.zapisz_do_pliku("sale_test.json")
-        system_plik = System()
-        system_plik.odczytaj_z_pliku("sale_test.json")
-
-        self.assertEqual(len(system_plik.sale), 2)
-        self.assertEqual(system_plik.sale[0].Numer, 1)
-        self.assertEqual(system_plik.sale[1].Numer, 2)
-        self.assertEqual(system_plik.sale[0].skladniki.skladniki[0]['skladnikmajatkowy'].nazwa, "Komputer")
-        self.assertEqual(system_plik.sale[1].skladniki.skladniki[0]['skladnikmajatkowy'].nazwa, "Projektor")
-
+    def test_przenies_skladnik(self):
+        sala1 = Sala(101)
+        sala2 = Sala(102)
+        self.system.dodaj_sale(sala1)
+        self.system.dodaj_sale(sala2)
+        self.system.dodaj_skladnik(101, 'sprzet', 'Komputer', 'Nowy')
+        skladnik_id = sala1.skladniki[0].id
+        wynik = self.system.przenies_skladnik(101, 102, skladnik_id)
+        self.assertIn('został przeniesiony', wynik)
+        self.assertEqual(len(sala1.skladniki), 0)
+        self.assertEqual(len(sala2.skladniki), 1)
+        self.assertEqual(sala2.skladniki[0].nazwa, 'Komputer')
 
 if __name__ == '__main__':
     unittest.main()
